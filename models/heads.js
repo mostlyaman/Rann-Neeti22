da51066b4a89df67f5d035fdaf9a959f7cc7ef46
Eagle <b20086@students.iitmandi.ac.in>
2022-09-19 16:35:39 +0530

    heads schema added

---
+++ models/heads.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const HeadSchema = new mongoose.Schema(
+    {
+        name:
+        {
+            type: String,
+            required: true,
+        },
+        title:
+        {
+            type: String,
+            required: true,
+        },
+        instagram:
+        {
+            type: String,
+        },
+        phone:
+        {
+            type: [Number],
+            required: true,
+        },
+        linkedin:
+        {
+            type: String,
+        },
+        twitter:
+        {
+            type: String,
+        },
+        image_link:
+        {
+            type: String,
+            required: true,
+        },
+    }
+);
+
+module.exports = mongoose.model("heads", HeadSchema);
